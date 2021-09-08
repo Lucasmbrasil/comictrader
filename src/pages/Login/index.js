c82258058cfea3136a9b4e5abaf055ba02a42df3
Lucas Brasil <lucasmbrasil2101@gmail.com>
2021-09-08 17:44:06 -0300

    feat(login): login page beta
    Login page beta done by Arthur Goulart

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,56 @@
 import React from "react";
 
+import axios from "axios";
+
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+
+import { useAuth } from "../../providers/auth";
 // import { Container } from './styles';
 
 function Login() {
-  return <div />;
+  const { authenticated, setAuthenticated } = useAuth(); // sera utilizado para proteção de rotas futuramente
+  const schema = yup.object().shape({
+    email: yup
+      .string()
+      .required("E-mail obrigatório")
+      .matches(/^(.+)@(\S+)$/, "Digite um formato de email valido"),
+    password: yup
+      .string()
+      .min(8, "Digite uma senha com 8 caracteres")
+      .required("Senha Obrigatoria")
+      .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
+        "uma letra maiuscula, uma letra minuscula e um numero"
+      ),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) });
+
+  const SignIN = (data) => {
+    console.log(data);
+    axios
+      .post("http://localhost:3001/login", data)
+      .then((response) => console.log(response))
+      .then((error) => console.log(error));
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(SignIN)}>
+        <input {...register("email")} placeholder="Digite seu email" />
+        {errors.email?.message}
+        <input {...register("password")} placeholder="Digite sua senha" />
+        {errors.password?.message}
+        <button type="submit">Logar com a minha conta</button>
+      </form>
+    </>
+  );
 }
 
 export default Login;
