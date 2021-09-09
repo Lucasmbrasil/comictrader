b16990e0fc1c4e9e1bbb19b6cd721e773599569f
Lucas Brasil <lucasmbrasil2101@gmail.com>
2021-09-09 09:57:29 -0300

    feat(dashboard/main): done searchbar

--- src/pages/Dashboard-Main/index.js
+++ src/pages/Dashboard-Main/index.js
@@ -5,6 +5,7 @@ import { Card, Container } from "./styled";
 // import { Container } from './styles';
 function DashboardMain() {
   const [hqs, setHqs] = useState([]);
+  const [input, setInput] = useState("");
 
   useEffect(() => {
     comic
@@ -14,17 +15,31 @@ function DashboardMain() {
       .then((response) => setHqs(response.data.results))
       .catch((e) => console.log(e));
   }, []);
+  const handleSearch = () => {
+    comic
+      .get(
+        `search/?api_key=bf2d39824c84c5c81e7f1adcabea036406aff8e9&format=json&sort=name:asc&resources=issue&query=${input}`
+      )
+      .then((response) => setHqs(response.data.results));
+  };
 
   console.log(hqs);
   return (
-    <Container>
-      {hqs.map((item) => (
-        <Card key={item.id}>
-          <div>{item.volume.name}</div>
-          <img src={item.image.thumb_url} alt={item} />
-        </Card>
-      ))}
-    </Container>
+    <>
+      <input
+        placeholder="Pesquise uma hq"
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={handleSearch}>Pesquisar</button>
+      <Container>
+        {hqs.map((item) => (
+          <Card key={item.id}>
+            <div>{item.volume.name}</div>
+            <img src={item.image.thumb_url} alt={item} />
+          </Card>
+        ))}
+      </Container>
+    </>
   );
 }
 export default DashboardMain;
