e63495319872cd15f258311707cf4d4f02699d8a
Arthur Goulart <arthurgoulartexec@gmail.com>
2021-09-09 16:09:45 -0300

    feat(made the dashboard comic page): made the dasboard comic page

--- src/pages/Dashboard-Comic/index.js
+++ src/pages/Dashboard-Comic/index.js
@@ -1,9 +1,73 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-// import { Container } from './styles';
+import comic from "../../services/comic"
+import axios from "axios";
+import { FatherContainer,Image,PageContent,ImageContainer,ContentContainer,TextContainer,TopBar ,WebSiteName,SearchDiv,SearchInput} from "./styles";
+import SearchIcon from '@material-ui/icons/Search';
+import { useState } from "react";
 
 function DashboardComic() {
-  return <div />;
+ const [comicObject,setComicImage]= useState([]);
+ const [description,setDescrition] = useState();
+ const [image,setImage] = useState();
+ 
+  useEffect(() => {
+    comic
+      .get(
+        "/issues/?api_key=bf2d39824c84c5c81e7f1adcabea036406aff8e9&format=json"
+      )
+      .then((response) => setComicImage([response.data.results[0]]))
+      .catch((e) => console.log(e));
+  }, []);
+
+useEffect(()=>{
+
+  
+  setDescrition(comicObject[0]?.description.replace(/<p>/g,"").replaceAll("</p>",""));
+ 
+
+  
+  
+},[comicObject])
+
+
+
+
+
+
+  return <>
+    <FatherContainer>
+      <TopBar>
+        <WebSiteName>
+          teste
+          <p>subtitulo</p>
+        </WebSiteName>
+            <div>Home</div>
+            <div>texto</div>
+            <div>texto</div>
+            <div>Lançamentos</div>
+            <SearchDiv>
+              <SearchIcon/><SearchInput/>
+            </SearchDiv>
+        </TopBar>
+        <PageContent>
+          <ContentContainer>
+            <TextContainer>
+              {comicObject?.map((titulo,index)=>(<h1 key={index}>{titulo.name}</h1>))}
+                <p>{description}</p>              
+              <div>
+              <button>Eu quero</button>
+              <button>Eu tenho</button>
+              </div>
+            </TextContainer>
+            <ImageContainer>
+              <Image src={comicObject[0]?.image?.small_url} alt=""/>
+              imagem aqui
+            </ImageContainer>
+          </ContentContainer>
+        </PageContent>
+    </FatherContainer>
+  </>;
 }
 
 export default DashboardComic;
