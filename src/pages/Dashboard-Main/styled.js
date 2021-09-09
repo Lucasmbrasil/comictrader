81258d92709146b99f0e1da5a94657f5de7e029e
Lucas Brasil <lucasmbrasil2101@gmail.com>
2021-09-09 09:31:45 -0300

    feat(dashboard/main): listing hqs
    change the url from services/comic and update the dashboard/main

---
+++ src/pages/Dashboard-Main/styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+export const Card = styled.div`
+  width: 120px;
+  height: 230px;
+  display: flex;
+  flex-wrap: wrap;
+  align-content: flex-end;
+  margin: 5px;
+`;
