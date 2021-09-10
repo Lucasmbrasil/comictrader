82494312ced3fd36b6c499c30038e99849cb1f92
Lucas Brasil <lucasmbrasil2101@gmail.com>
2021-09-10 01:28:44 -0300

    feat(dashboard/main): update some features

--- src/pages/Dashboard-Main/index.js
+++ src/pages/Dashboard-Main/index.js
@@ -1,17 +1,20 @@
 import { useEffect, useState } from "react";
 import comic from "../../services/comic";
 import { Card, Container } from "./styled";
-
+import { useHistory } from "react-router-dom";
 // import { Container } from './styles';
 function DashboardMain() {
   const [hqs, setHqs] = useState([]);
   const [input, setInput] = useState("");
+  const [description, setDescription] = useState([]);
   useEffect(() => {
     comic
       .get(
         "/issues/?api_key=bf2d39824c84c5c81e7f1adcabea036406aff8e9&format=json"
       )
-      .then((response) => setHqs(response.data.results))
+      .then((response) => {
+        setHqs(response.data.results);
+      })
       .catch((e) => console.log(e));
   }, []);
   const handleSearch = () => {
@@ -22,8 +25,12 @@ function DashboardMain() {
       .then((response) => setHqs(response.data.results))
       .catch((e) => console.log(e));
   };
+  // &resources=character&query=${input}
+  // api_key=bf2d39824c84c5c81e7f1adcabea036406aff8e9&format=json
 
-  console.log(hqs);
+  // `https://comicvine.gamespot.com/api/issue/4000-${id}/?api_key=bf2d39824c84c5c81e7f1adcabea036406aff8e9&format=json`
+  console.log(description);
+
   return (
     <>
       <input
@@ -35,8 +42,12 @@ function DashboardMain() {
       <Container>
         {hqs.map((item) => (
           <Card key={item.id}>
+            {/* {console.log(item.api_detail_url)} */}
+            {setDescription([...description, item.description])}
+
             <div>{item.volume.name}</div>
             <img src={item.image.thumb_url} alt={item} />
+            {`${item.description}`}
           </Card>
         ))}
       </Container>
