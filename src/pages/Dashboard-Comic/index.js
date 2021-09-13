aed48d6e0994f24e2ed07608b75e349e293ac67a
Anna Louise Oliveira <anna.louise@hotmail.com>
2021-09-13 17:49:21 -0300

    feat(pages/dashboard-comic): page styled and responsive

--- src/pages/Dashboard-Comic/index.js
+++ src/pages/Dashboard-Comic/index.js
@@ -1,23 +1,17 @@
 import React, { useEffect } from "react";
-
-import comic from "../../services/comic";
-import axios from "axios";
 import {
-  FatherContainer,
   Image,
-  PageContent,
   ImageContainer,
-  ContentContainer,
-  TextContainer,
-  TopBar,
-  WebSiteName,
-  SearchDiv,
-  SearchInput,
+  ComicBackground,
+  DashboardComicContainer,
+  ComicMainContainer,
+  InfoContainer,
 } from "./styles";
-import SearchIcon from "@material-ui/icons/Search";
 import { useState } from "react";
 import { useComics } from "../../providers/comics";
 import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import { DashboardBackground, DashboardContainer } from "../../styles/globalComponents";
 
 function DashboardComic() {
   const [comicObject, setComicImage] = useState([]);
@@ -27,40 +21,48 @@ function DashboardComic() {
 
   useEffect(() => {
     getComic(id);
-  }, [id]);
+  }, []);
 
   return (
-    <>
-      <FatherContainer>
-        <TopBar>
-          <Header />
-        </TopBar>
-        <PageContent>
-          <ContentContainer>
-            {specificComic.aliases !== undefined && (
-              <>
-                <TextContainer>
-                  <h1> {specificComic.volume.name}</h1>
+      <DashboardBackground>
+        <Header />
+          {specificComic.aliases !== undefined && (
+            <DashboardComicContainer>
+              <ComicBackground style={{
+                background: `url(${specificComic.image.small_url}) no-repeat center`,
+                backgroundSize: "cover"
+                }}>
+                <div className="ComicBlackTop">
+                  <img src={specificComic.image.thumb_url} alt={specificComic.volume.name} />
+                  <h1>{specificComic.volume.name}</h1>
                   <p>
                     {specificComic.description !== null &&
-                      specificComic.description
-                        .slice(0, specificComic.description.indexOf("<h4>"))
-                        .replace(/<.*?>/g, " ")}
+                      (specificComic.description.includes("<h4>")
+                      ? specificComic.description
+                          .slice(0, specificComic.description.indexOf("<h4>"))
+                          .replace(/<.*?>/g, " ")
+                      : specificComic.description.replace(/<.*?>/g, " "))}
                   </p>
-                  <div>
-                    <button>Eu quero</button>
-                    <button>Eu tenho</button>
-                  </div>
-                </TextContainer>
-                <ImageContainer>
-                  <Image src={specificComic.image.small_url} alt="" />
-                </ImageContainer>
-              </>
-            )}
-          </ContentContainer>
-        </PageContent>
-      </FatherContainer>
-    </>
+                <div className="ComicButtons">
+                  <button>Eu quero</button>
+                  <button>Eu tenho</button>
+                </div>
+                </div>
+              </ComicBackground>
+              <InfoContainer>
+                <div className="WhoHas">
+                  <h3>Quem tem esta HQ:</h3>
+                  <div></div>
+                </div>
+                <div className="WhoWants">
+                  <h3>Quem também quer:</h3>
+                  <div></div>
+                </div>
+              </InfoContainer>
+            </DashboardComicContainer>
+          )}
+        <Footer/>
+      </DashboardBackground>
   );
 }
 
--- src/pages/Dashboard-Comic/styles.js
+++ src/pages/Dashboard-Comic/styles.js
@@ -1,79 +1,104 @@
 import styled from "styled-components";
 
-
-export const FatherContainer = styled.div`
-    background-color:red;
-    height:100vh;
-    width :100%;
-`
-
-
-export const TopBar = styled.div`
-    background-color:green;
-    height:10%;
-    width :100%;
-    display:flex;
-    justify-content: space-between;
-    align-items: center;
-`
-
-
-export const WebSiteName = styled.div`
-    background-color:blue;
- width: 33%;
-    height: 100%;
-`
-
-export const SearchDiv = styled.div`
-    border-radius: 5px;
-    border: 1px solid black;
-    width: 14%;
+export const DashboardComicContainer = styled.div`
+    width: 90vw;
+    min-height: 60vh;
     display: flex;
-    justify-content: space-evenly;
-`;
-
-export const SearchInput = styled.input`
-    border:none;
-`;
-
-
-export const PageContent = styled.div`
-    background-color:purple;
-    height:100%;
-    display:flex;
-    justify-content:center;
+    flex-wrap: wrap;
+    flex: 1 0 auto;
+    flex-wrap: wrap;
     align-items: center;
-`;
+    margin-block: 20px;
+    justify-content: space-around;
+    padding: 15px 0;
+`
 
-export const ContentContainer = styled.div`
-    background-color:blue;
-    height:50%;
-    width: 85%;
-    display:flex;
-`;
-
-
-export const TextContainer = styled.div`
-   background-color: red;
-    width: 64%;
+export const ComicBackground = styled.div`
     display: flex;
-    flex-direction: column;
+    justify-content: center;
     align-items: center;
-    justify-content: space-evenly;
-    p{
-        text-align:center;
+    min-width: 40vw;
+    max-width: 400px;
+    background-size: cover;
+    
+    .ComicBlackTop{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        min-height: 60vh;
+        height: 100%;
+        padding: 15px 0;
+        background-color: rgb(0,0,0,0.7);
+
+        img {
+            border: 2px solid white;
+            border-radius: 5px;
+        }
+
+        h1 {
+            font-size: 38px;
+            font-family: 'Staatliches', cursive;
+            font-weigth: 200;
+            width: 80%;
+        }
+    
+        p {
+            font-size: 14px;
+            font-family: 'Urbanist', sans-serif;
+            width: 80%;
+            margin-inline: 0;
+        }
     }
 
-`;
+    .ComicButtons {
+        button {
+            padding: 10px 15px;
+            border-radius: 5px;
+            background-color: red;
+            color: white;
+            cursor: pointer;
+            outline: none;
+            border: none;
+            font-size: 16px;
+            font-family: 'Urbanist', sans-serif;
+            margin-inline: 10px;
+          }
+        
+          button:hover {
+            background-color: black;
+            transition:0.3s;
+          }
+        
+          button:active {
+            background-color: white;
+            transition:0.3s;
+            color: black;
+          }        
+    }    
+`
 
-export const ImageContainer = styled.div`
-    background-color: orange;
-    width: 36%;
+export const InfoContainer = styled.div`
+    display: flex;
+    margin-block: 20px;
+    justify-content: space-around;
+    align-items: flex-start;
+    min-width: 40vw;
+    max-width: 500px;
+    background-size: cover;
+    background-color: white;
+    padding: 12px;
+    min-height: 60vh;
+    box-shadow: 2px 2px 6px 0px rgba(0,0,0,0.5);
 
-`;
+    h3 {
+        color: black;
+        font-size: 24px;
+        font-family: 'Staatliches', cursive;
+    }
 
-export const Image = styled.img`
-    width: 15%;
-    height: 15%;
+    .WhoHas {
+        margin-inline-end: 20px;
+    }
+`
 
-`;
